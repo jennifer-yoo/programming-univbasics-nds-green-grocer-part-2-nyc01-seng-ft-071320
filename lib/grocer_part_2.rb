ae383419f205c4111370c4a7af266d0d01d67690
Jennifer Yoo <jennifer.yoo7@gmail.com>
2020-07-12 02:58:16 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -50,8 +50,8 @@ def checkout(cart, coupons)
   grand_total =
   counter = 0
 
-  while counter < final_cart.length
-      grand_total = final_cart[counter][:price] * final_cart[counter][:count]
+  while counter < coupon_clearance_cart.length
+      grand_total = coupon_clearance_cart[counter][:price] * coupon_clearance_cart[counter][:count]
         if grand_total > 100
           grand_total -= (grand_total * 0.1)
         end

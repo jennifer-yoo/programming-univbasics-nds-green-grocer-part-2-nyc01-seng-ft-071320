e122b449c167c82bbbe4b7f85184cee1c00397e3
Jennifer Yoo <jennifer.yoo7@gmail.com>
2020-07-12 00:44:41 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,9 +1,16 @@
+require 'pry'
+
 require_relative './part_1_solution.rb'
 
 def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  new_array = []
+
+  cart.each do |item_list|
+    item_list.each do |item_info|
+      binding.pry
+
+    end
+  end
 end
 
 def apply_clearance(cart)
